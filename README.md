87b428fecbc4342de2c70eb61dd24c1084697a77
Rahula Palu <rahula_dd@hotmail.com>
2022-09-25 16:54:15 +0100

    v1
    Online version.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# unfollowers
+# Check Unfollowers Instagram Online
 Code used to create Check Unfollowers Instagram Online website
+https://unfollowersonline.com
+Created by me: Rahula :)
+
+# How it works
+Our website simply works using coding "text comparison" between two fields, with the language selected.
+Does not exist database during this method. The only database is from WordPress CMS.
+
+# How it works - DETAILED (in 8 steps)
+1. The main functionality is contained in just one page using HTML + PHP without database.
+2. First, that page have a video explaining how to operate both input fields and language selector for the users convenience.
+3. Right below, this language selector mentioned is <b>necessary</b> to understand what's the language of your browser, and eventually (and most common) of your instagram as well.
+4. When you copy and paste your followers, for example, you can notice some phrase like "XXXXXXXXXX's profile picture".
+5. That way, combining your language selected and our predefined sentences, we are able to separate what is the "XXXXXXXXXX" username from "'s profile picture" common sentence in several languages.
+6. And like that we can have a list of all usernames of your input without duplicating.
+7. Now we make the comparison between the usernames from the list 1 (input 1 - followers) and from the list 2 (input 2 - following).
+8. Then you have a fulfilled table of who doesn't follow you back.
+9. Due to copy and paste process, you aren't able to do it on mobile app or devices (more difficult).
+
+# Theme - WordPress
+Although is not related with that comparison work, for that website was used:
+WordPress - CMS v6.+
+Astra Free Theme
+
+# Credits
+I hope it fit for you and feel free to do good contribute, which will be checked of course.
+Created by me: Rahula Palu :)
---
+++ page-home.php
@@ -0,0 +1,287 @@
+<?php /* Template Name: Home */ ?>
+
+<?php
+// if ( !is_user_logged_in() ) {
+// 	wp_redirect( home_url() );
+// } 
+?>
+<head>
+	<meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+    <?php do_action( 'wpseo_head' ); ?>
+  	
+    <!-- Favicons -->
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/img/favicon.png" rel="icon">
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/img/favicon.png" rel="apple-touch-icon">
+  
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
+  
+    <!-- Vendor CSS Files -->
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  
+    <!-- Template Main CSS File -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link href="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/css/style.css" rel="stylesheet">
+	
+
+</head>
+<style>
+	#table-result td{
+		min-width:150px;
+		padding:4px 10px 4px 0px;
+		
+	}
+	#table-result td a{
+		letter-spacing:1px;
+	}
+</style>  
+  <body>
+  
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top d-flex align-items-center">
+      <div class="container d-flex align-items-center justify-content-between">
+  
+        <div id="logo">
+          <!-- <h1><a href="index.html"><span>e</span>Startup</a></h1> -->
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/img/svg/Logo Unfollowers.svg" style="width:40vw;max-width:250px;margin-left:20px;" alt="Logo Unfollowers Instagram Online" title="" /></a>
+        </div>
+  
+        <nav id="navbar" class="navbar">
+          <h2 style="font-family: 'Philosopher', sans-serif; color: #f26101;text-align:right">No app, login or password required!</h2>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+  
+      </div>
+    </header><!-- End Header -->
+  
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+      <div class="hero-container" data-aos="fade-in">
+        <h1 style="font-size:36px;">Check Unfollowers Instagram Online</h1>
+        <h2 style="margin-bottom:0">Checks who don't follow you back on Instagram!</h2>
+        <p><strong><u>Follow the instructions below.</u></strong> Works only using your browser.</p>
+      	<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="600" height="400" style="width:90%;max-width:600px;" type="text/html" src="https://www.youtube.com/embed/LbmBREr9oSU?autoplay=1&fs=1&iv_load_policy=1&showinfo=0&rel=0&cc_load_policy=1&start=0&end=0&origin=http://youtubeembedcode.com"></iframe>
+        <a href="#get-started" class="btn btn-success scrollto" style="font-size:1.2em;color:white;font-weight:700;margin-top:15px;">Get Started</a>
+      </div>
+    </section><!-- End Hero Section -->
+  
+    <main id="main">
+  
+      <!-- ======= Get Started Section ======= -->
+      <section id="get-started" class="padd-section text-center" style="height:auto;max-height:unset;padding-bottom:30px;">
+  
+        <div class="container" data-aos="fade-up">
+          <div class="section-title text-center" style="margin-bottom:20px">
+  
+            <h2 style="margin-bottom:0;padding-top:5px;">Copy and Paste</h2>
+			  <p style="margin-bottom:5px;">Current supported languages: <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/it.png" height="24" width="24" alt="it" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/en-us.png" height="24" width="24" alt="en" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/pt-br.png" height="24" width="24" alt="pt" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/es.png" height="24" width="24" alt="es" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/ru.png" height="24" width="24" alt="ru" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/fr.png" height="24" width="24" alt="fr" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/de.png" height="24" width="24" alt="de" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/sv.png" height="24" width="24" alt="sv" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/vi.png" height="24" width="24" alt="vi" /> <img src="<?php echo get_site_url(); ?>/wp-content/plugins/gtranslate/flags/24/nl.png" height="24" width="24" alt="nl" ></p>
+			  <p style="margin-bottom:0;">Check that the language is correct: <em>(important for comparison - if it's wrong, may not work)</em></p>
+			  <div><div><?php echo do_shortcode('[gtranslate]'); ?></div></div>
+            <p class="separator">Your Followers and Following users - WE DO NOT KEEP ANY DATA</p>
+  
+          </div>
+        </div>
+        <form action="#result" method="POST">
+		<input type="hidden" name="currentlang" id="hidden_currentlang" value="">
+        <div class="container" style="padding-left:10vw;padding-right:10vw">
+          <div class="row" >
+            
+            <div class="col-md-6 col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+              <div class="feature-block">
+                <h4 style="margin-bottom:0">FOLLOWERS</h4>
+                <p>Paste here who does follow you!</p>
+                <textarea rows="8" class="form-control" name="followers" id="followers" required></textarea>
+              </div>
+            </div>
+  
+            <div class="col-md-6 col-lg-6" data-aos="zoom-in" data-aos-delay="200">
+              <div class="feature-block">
+                <h4 style="margin-bottom:0">FOLLOWING</h4>
+                <p>Paste those you follow here!</p>
+                <textarea rows="8" class="form-control" name="following" id="following" required></textarea>
+  
+              </div>
+            </div>
+          </div>
+         
+        </div>
+        <div class="container">
+        <div class="row" style="padding-top:15px;">
+            <div class="col-md-6 offset-md-3 offset-lg-3 col-lg-6" data-aos="zoom-in" data-aos-delay="300">
+                <input type="submit" class="btn-info form-control" name="start" id="start" style="color:white;font-weight:700;" value="START COMPARISON">
+            </div>
+  
+          </div>
+        </div>
+      </form>
+      </section><!-- End Get Started Section -->
+  
+     <section id="result">
+     <div class="container" style="padding-left:10vw;padding-right:10vw">
+      <div class="row" style="padding-top:15px;padding-bottom:15px;">
+        <?php 
+// 		  receiving data
+            if(isset($_POST['start'])){
+				
+// 				construct methods
+// 				comparison general function which string receives the text
+				function CompGeneral($resultado,$string){
+					$auxarr=array();
+					$auxfor=1;
+					foreach ($resultado as $key => $value) {
+						if (stripos($value, $string) !== false) {
+							array_push($auxarr,str_replace($string,"",$value));
+							
+						}
+					}
+					sort($auxarr);
+					foreach ($auxarr as $key => $val) {
+						if($auxfor%5===0){
+								echo "</tr><tr>";
+						}
+						echo '<td><a href="https://instagram.com/'.str_replace($string,"",$val).'" target="_blank" rel="nofollow"><span translate="no">'.str_replace($string,"",$val).'</span></a></td>';
+						$auxfor++;
+					}
+					global $cont_aux;
+					$cont_aux=count($auxarr);
+				}
+				
+				
+				if(!empty(wp_strip_all_tags($_POST['followers'])) && !empty(wp_strip_all_tags($_POST['following']))){
+              $lines1aux = explode("\n", str_replace("\r", "",wp_strip_all_tags($_POST['followers'])));
+                $lines2aux = explode("\n", str_replace("\r", "",wp_strip_all_tags($_POST['following'])));
+                
+				echo '<table class="table-result" id="table-result" style="overflow: scroll !important;flex-wrap: nowrap !important;justify-content: unset !important;display: block;"><tr>';
+                $result = array_diff( $lines2aux, $lines1aux );
+				
+				
+				if((strcmp(wp_strip_all_tags($_POST['currentlang']),'en')===0) or (strcmp(wp_strip_all_tags($_POST['currentlang']),'null')===0)){
+// 					english
+					$string="\'s profile picture";
+				}elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'it')===0){
+// 					italian
+					$string="Immagine del profilo di ";
+				}elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'nl')===0){
+// 					nederlands
+					$string="Profielfoto van";
+				}
+				elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'fr')===0){
+// 					french
+					$string="Photo de profil de ";
+				}
+				elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'de')===0){
+// 					german
+					$string=" Profilbild";
+				}
+				elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'pt')===0){
+// 					portuguese
+					$string="Foto de perfil de ";
+				}
+				elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'ru')===0){
+// 					russian
+					$string="Фото профиля ";
+				}
+				elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'es')===0){
+// 					spanish
+					$string="Foto del perfil de ";
+				}
+				elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'sv')===0){
+// 					swedish i guess
+					$string=" profilbild";
+				}
+				elseif(strcmp(wp_strip_all_tags($_POST['currentlang']),'vi')===0){
+// 					tieng viet
+					$string="Ảnh đại diện của ";
+				}
+				CompGeneral($result,$string);
+					
+					
+				echo '<h3 style="margin-bottom:20px">These users below do not follow you. Total of: <span style="color:darkorange">'.$cont_aux.'</span> users.</h3>';
+				?>
+		  			<input type="text" id="inputfilter" placeholder="Search for usernames.." title="Type in a username" style="background-image: url('https://unfollowersonline.com/wp-content/uploads/2021/12/searchicon.png');background-position: 10px 10px;background-repeat: no-repeat;width: 100%;font-size: 16px;padding: 12px 20px 12px 40px;border: 1px solid #ddd;margin-bottom: 12px;">
+		  		<?php
+				echo '</table>';
+				}else{
+					echo '<h3 style="margin-bottom:20px">You must paste your text. If you need help, follow <a href="#hero">this tutorial.</a></h3>';
+				}
+            }
+
+        ?>
+      </div>
+    </div>
+     </section>
+  
+    </main><!-- End #main -->
+  
+    <!-- ======= Footer ======= -->
+    <footer class="footer">
+  
+      <div class="copyrights">
+        <div class="container">
+			<?php echo do_shortcode('[gtranslate]'); ?>
+          <p>&copy; Copyrights Unfollowers Online. All rights reserved.</p>
+          <div class="credits">
+            Designed by Unfollowers Online ♥ and Rahula P.C.
+            <br/><a href="<?php echo get_privacy_policy_url(); ?>">Privacy Policy</a>
+          </div>
+        </div>
+      </div>
+  	<div id="cookieConsent">
+    <div id="closeCookieConsent">x</div>
+    This website is using cookies. <a href="<?php echo get_privacy_policy_url(); ?>" target="_blank">More info</a>. <a class="cookieConsentOK">OK</a>
+</div>
+    </footer><!-- End  Footer -->
+  
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  
+    <!-- Vendor JS Files -->
+    <script src="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/aos/aos.js"></script>
+    <script src="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/vendor/php-email-form/validate.js"></script>
+	  <script>
+	  function changeLanguageform(current){
+		  document.querySelector('#hidden_currentlang').setAttribute('value',current);
+	  }
+	  </script>
+	  <script>
+	  var carname = GTranslateGetCurrentLang();
+		console.log(carname);
+		changeLanguageform(carname);  
+	  </script>
+	  <script>
+	 $(document).ready(function(){   
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    }); 
+}); 
+	  </script>
+<script>
+$(document).ready(function(){
+  $("#inputfilter").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#table-result tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+    <!-- Template Main JS File -->
+    <script src="<?php echo get_site_url().'/wp-content/themes/astra/inc/n-home/';?>assets/js/main.js"></script>
+  	 <script>
+			$(document).ready(function(){
+			  $('#tooltip').tooltip();   
+			});
+	  </script>
+  </body>
